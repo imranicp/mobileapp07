3158e41e2df8a4fa2459f3ffac845d7d636ab157
i1990jain <i1990jain@gmail.com>
2016-01-12 14:17:36 +0100

    fixed Name Layout

--- app/src/main/java/logic/main/com/boardgame/ListViewAdapter.java
+++ app/src/main/java/logic/main/com/boardgame/ListViewAdapter.java
@@ -23,25 +23,23 @@ public class ListViewAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        // TODO Auto-generated method stub
+
         return list.size();
     }
 
     @Override
     public Object getItem(int position) {
-        // TODO Auto-generated method stub
+
         return list.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // TODO Auto-generated method stub
 
 
         LayoutInflater inflater = activity.getLayoutInflater();
