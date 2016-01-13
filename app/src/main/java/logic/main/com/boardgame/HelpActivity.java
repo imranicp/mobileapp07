9953f530af5b6e929c0083c96dcbf0e05ed944d5
imranicp <imran9392008@gmail.com>
2016-01-13 04:48:15 +0100

    History Layout replaced with Gallery Like Swipes (Unfinished), Score Layout Adjusted, History Layout Adjusted. Library Added in Gradle.

--- app/src/main/java/logic/main/com/boardgame/HelpActivity.java
+++ app/src/main/java/logic/main/com/boardgame/HelpActivity.java
@@ -2,29 +2,66 @@ package logic.main.com.boardgame;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 
+import com.viewpagerindicator.CirclePageIndicator;
+import com.viewpagerindicator.PageIndicator;
+
+
+/**
+ * Created by Vahiiid on 1/11/2016.
+ */
 public class HelpActivity extends Activity {
-    boolean continueMusic;
+
+    // Declare Variables
+    ViewPager viewPager;
+    PagerAdapter adapter;
+    String[] number;
+    String[] desc;
+    int[] image;
+    PageIndicator mIndicator;
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        if (!continueMusic) {
-            MusicManager.pause();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        continueMusic = false;
-        MusicManager.start(this, MusicManager.MUSIC_MENU);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.help_layout);
-        continueMusic = getIntent().getExtras().getBoolean("continueMusic");
+        // Get the view from viewpager_main.xml
+        setContentView(R.layout.viewpager_main);
+
+        // Generate sample data
+        number = new String[] { "1", "2", "3", "4", "5", "6", "7"};
+
+        desc = new String[] { "Description About First Page,Description About First Page,Description About First Page,",
+                "Description About First Page,Description About First Page,Description About First Page,",
+                "Description About First Page,Description About First Page,Description About First Page,",
+                "Description About First Page,Description About First Page,Description About First Page,",
+                "Description About First Page,Description About First Page,Description About First Page,",
+                "Description About First Page,Description About First Page,Description About First Page,",
+                "Description About First Page,Description About First Page,Description About First Page,"};
+
+
+        image = new int[] { R.drawable.hlp1, R.drawable.hlp2,
+                R.drawable.hlp3d, R.drawable.hlp3l,
+                R.drawable.hlp3r, R.drawable.hlp3u, R.drawable.hlp4,};
+
+        // Locate the ViewPager in viewpager_main.xml
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        // Pass results to ViewPagerAdapter Class
+        adapter = new ViewPagerAdapter(HelpActivity.this, number, desc, image);
+        // Binds the Adapter to the ViewPager
+        viewPager.setAdapter(adapter);
+
+        CirclePageIndicator indicator = (CirclePageIndicator)findViewById(R.id.indicator);
+        mIndicator = indicator;
+        indicator.setViewPager(viewPager);
+
+        final float density = getResources().getDisplayMetrics().density;
+        indicator.setBackgroundColor(0x00000000);
+        indicator.setRadius(10 * density);
+        indicator.setPageColor(0xFFf0f8ff);
+        indicator.setFillColor(0x88bc8861);
+        indicator.setStrokeColor(0xFF000000);
+        indicator.setStrokeWidth(2 * density);
+
     }
 }
